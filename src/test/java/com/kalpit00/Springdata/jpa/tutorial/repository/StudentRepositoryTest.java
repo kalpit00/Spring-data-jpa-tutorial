abc75eb3a88c56b603a452871421680cba44f6c0
kalpit00 <kalpitpatel@Kalpits-MacBook-Air.local>
2024-07-11 07:42:23 +0530

    Embed Guardian obj into Student obj and update Unit Test accordingly

--- src/test/java/com/kalpit00/Springdata/jpa/tutorial/repository/StudentRepositoryTest.java
+++ src/test/java/com/kalpit00/Springdata/jpa/tutorial/repository/StudentRepositoryTest.java
@@ -1,5 +1,6 @@
 package com.kalpit00.Springdata.jpa.tutorial.repository;
 
+import com.kalpit00.Springdata.jpa.tutorial.entity.Guardian;
 import com.kalpit00.Springdata.jpa.tutorial.entity.Student;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,11 +15,20 @@ class StudentRepositoryTest {
     @Autowired
     private StudentRepository studentRepository;
 
+//    @Test
+//    public void saveStudent() {
+//        Student student = Student.builder().emailId("kalpit@gmail.com").firstName("Kalpit").
+//            lastName("Patel").guardianName("Father").guardianEmail("Father@gmail.com")
+//            .guardianMobile("123456789").build();
+//        studentRepository.save(student);
+//    }
+
     @Test
-    public void saveStudent() {
-        Student student = Student.builder().emailId("kalpit@gmail.com").firstName("Kalpit").
-            lastName("Patel").guardianName("Father").guardianEmail("Father@gmail.com")
-            .guardianMobile("123456789").build();
+    public void saveStudentWithGuardian() {
+        Guardian guardian = Guardian.builder().name("Father").email("Father@gmail.com").
+                mobile("123456789").build();
+        Student student = Student.builder().emailId("kalpit2@gmail.com").firstName("Kalpit").
+                lastName("Patel").guardian(guardian).build();
         studentRepository.save(student);
     }
 
