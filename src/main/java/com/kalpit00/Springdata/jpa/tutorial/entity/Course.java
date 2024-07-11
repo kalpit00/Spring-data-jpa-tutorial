0f23759ef7682c705c29c82e8f36956a40caf26a
kalpit00 <kalpitpatel@Kalpits-MacBook-Air.local>
2024-07-11 11:20:41 +0530

    How to add another Entity as a Foreign Key in another Entity's Table

---
+++ src/main/java/com/kalpit00/Springdata/jpa/tutorial/entity/Course.java
@@ -0,0 +1,28 @@
+package com.kalpit00.Springdata.jpa.tutorial.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Course {
+    @Id
+    @SequenceGenerator(
+            name = "course_sequence",
+            sequenceName = "course_sequence",
+            allocationSize = 1
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "course_sequence"
+    )
+    private Long courseId;
+    private String courseTitle;
+    private Integer credit;
+}
